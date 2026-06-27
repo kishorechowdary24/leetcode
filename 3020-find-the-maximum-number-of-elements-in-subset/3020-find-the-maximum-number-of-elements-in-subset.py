51e8ce7e030ae1ffaf7eea43919ad593d8312ba0
kishore chowday <kishorechowdary7788@gmail.com>
2026-06-27 10:12:58 +0530

    Time: 134 ms (47.24%), Space: 31.8 MB (38.58%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.py
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.py
@@ -1,37 +1,23 @@
-from typing import List
-from collections import Counter
-
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
-        freq = Counter(nums)
-        ans = 1
+        fm = defaultdict(int)
+        for num in nums: fm[num] += 1
 
-        # Special case for 1
-        if 1 in freq:
-            if freq[1] % 2 == 0:
-                ans = max(ans, freq[1] - 1)
-            else:
-                ans = max(ans, freq[1])
+        ones = fm[1]
+        res = ones if ones % 2 == 1 else max(0, ones - 1)
 
-        for x in freq:
-            if x == 1:
-                continue
+        for num in fm:
+            if num == 1: continue
 
-            cur = x
-            length = 0
+            total = 0
+            curr = num
 
-            # Every level except the last needs two copies
-            while freq.get(cur, 0) >= 2:
-                length += 2
-                cur = cur * cur
+            while curr in fm and fm[curr] >= 2:
+                total += 2
+                curr *= curr
 
-            # Middle element exists
-            if freq.get(cur, 0) >= 1:
-                length += 1
-            else:
-                # Remove one pair because no center exists
-                length -= 1
-
-            ans = max(ans, length)
-
-        return ans
+            if curr in fm: total += 1
+            elif not curr in fm: total -= 1
+            res = max(res, total)
+        
+        return res
