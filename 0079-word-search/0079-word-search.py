87f2e8ca157cb2261fb2e5ab757ce40dec5c549d
kishore chowday <kishorechowdary7788@gmail.com>
2026-07-07 23:15:40 +0530

    Time: 3376 ms (82.12%), Space: 19.6 MB (25.88%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,40 @@
 class Solution:
     def exist(self, board, word):
-        m, n = len(board), len(board[0])
 
-        def dfs(i, j, k):
-            if k == len(word):
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(i, j, index):
+
+            if index == len(word):
                 return True
 
-            if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != word[k]:
+            # Boundary check
+            if i < 0 or i >= rows or j < 0 or j >= cols:
+                return False
+
+            if board[i][j] != word[index]:
                 return False
 
             temp = board[i][j]
-            board[i][j] = "#"
 
-            res = (dfs(i+1, j, k+1) or
-                   dfs(i-1, j, k+1) or
-                   dfs(i, j+1, k+1) or
-                   dfs(i, j-1, k+1))
+            board[i][j] = "$"
+
+            found = (
+                dfs(i, j + 1, index + 1) or   # Right
+                dfs(i, j - 1, index + 1) or   # Left
+                dfs(i + 1, j, index + 1) or   # Down
+                dfs(i - 1, j, index + 1)      # Up
+            )
 
             board[i][j] = temp
-            return res
 
-        for i in range(m):
-            for j in range(n):
-                if dfs(i, j, 0):
-                    return True
+            return found
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    if dfs(i, j, 0):
+                        return True
 
         return False
