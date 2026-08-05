9f294a18e43f9aeac8d1b4527ad0653e41e37baf
kishore chowday <kishorechowdary7788@gmail.com>
2026-08-05 21:27:00 +0530

    Time: 261 ms (53.53%), Space: 30.6 MB (87.57%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -7,13 +7,11 @@ class Solution:
         ans = 1
 
         for right in range(len(nums)):
-
             total += nums[right]
 
             while nums[right] * (right - left + 1) > total + k:
                 total -= nums[left]
                 left += 1
-
+            
             ans = max(ans, right - left + 1)
-
         return ans
