69c9d644ccec27300cd8e6be9e6f3a5dec0e233f
kishore chowday <kishorechowdary7788@gmail.com>
2026-09-01 12:51:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,45 +11,30 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
+       ListNode dummy = new ListNode(0);
+       ListNode curr = dummy;
 
-        while(l1 != null){
-            list1.add(l1.val);
-            l1 = l1.next;
+       int carry = 0;
+
+       while (l1 != null || l2 != null || carry != null){
+        int val1 , val2;
+        if (l1 != null){
+             val1 = l1.val;
+        }
+        else{
+            val1 = 0;
         }
 
-        while (l2 != null){
-            list2.add(l2.val);
-            l2 = l2.next;
+        if (l2 != null){
+            val2 = l2.val;
+        }
+        else{
+            val2 = 0;
         }
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        int i = 0, j = 0, carry = 0;
+        int sum = val1 + val2 + carry;
 
-        while(i < list1.size() || j < list2.size() || carry != 0){
-            int sum = carry;
-
-            if (i < list1.size()){
-                sum += list1.get(i++);
-            }
-            if (j < list2.size()){
-                sum += list2.get(j++);
-            }
-
-            ans.add(sum % 10);
-            carry = sum / 10;
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-
-        for(int digit: ans){
-
-            curr.next = new ListNode(digit);
-            curr = curr.next;
-        }
-
-        return dummy.next;
-
+        caryy = sum / 10;
+       }
     }
 }
