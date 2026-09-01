5996a1be0437c66e82cdd4aafbf59a4119debbe4
kishore chowday <kishorechowdary7788@gmail.com>
2026-09-01 12:40:10 +0530

    Time: 2 ms (5.31%), Space: 46.4 MB (57.94%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,29 +10,46 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+        
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
 
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            int val1 = (l1 != null) ? l1.val : 0;
-            int val2 = (l2 != null) ? l2.val : 0;
-
-            // Add the values and carry
-            int sum = val1 + val2 + carry;
-
-            // New carry for next iteration
-            carry = sum / 10;
-
-            // Create a new node with the remainder and attach it
-            current.next = new ListNode(sum % 10);
-            current = current.next;
-
-            // Move l1 and l2 to the next nodes
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+        while(l1 != null){
+            list1.add(l1.val);
+            l1 = l1.next;
         }
 
-        return dummy.next; // Skip dummy and return head of result list
+        while (l2 != null){
+            list2.add(l2.val);
+            l2 = l2.next;
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0, j = 0, carry = 0;
+
+        while(i < list1.size() || j < list2.size() || carry != 0){
+            int sum = carry;
+
+            if (i < list1.size()){
+                sum += list1.get(i++);
+            }
+            if (j < list2.size()){
+                sum += list2.get(j++);
+            }
+
+            ans.add(sum % 10);
+            carry = sum / 10;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        for(int digit: ans){
+
+            curr.next = new ListNode(digit);
+            curr = curr.next;
+        }
+
+        return dummy.next;
+
     }
 }
